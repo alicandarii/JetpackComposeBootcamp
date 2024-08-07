5ad9c1cd0fe8fee47f7c32991e9a7b6ad3445219
alicandarii <alicandarii@icloud.com>
2024-08-07 19:38:53 +0300

    Odev-1

---
+++ Odev1.kt
@@ -0,0 +1,99 @@
+package com.example.kotlindersleri.degiskenler
+
+fun main() {
+
+    val sehir = "Kayseri"
+    println(sehir)
+    val ulke = "Türkiye"
+    println(ulke)
+    val tel = "05511067134"
+    println(tel)
+    val postaKodu = "38038"
+    println(postaKodu)
+    val email = "alicandarii@icloud.com"
+    println(email)
+    val meslek = "Bilgisayar Mühendisi"
+    println(meslek)
+    val stok = 100
+    println(stok)
+    val musteriAdi = "Ali Can"
+    println(musteriAdi)
+    val bakiye = 500
+    println(bakiye)
+    val dogumGunu = "20/04/2000"
+    println(dogumGunu)
+    val maas = 30000
+    println(maas)
+    val medeniDurum = "Bekar"
+    println(medeniDurum)
+    val urunYorum = " lorem ipsum"
+    println(urunYorum)
+    val odemeTarihi = "15/08/2024"
+    println(odemeTarihi)
+    val odeme = "nakit"
+    println(odeme)
+    val siparisAdet = 20
+    println(siparisAdet)
+    val arabaModeli = "Ford Focus"
+    println(arabaModeli)
+    val kitapAdi = "Monte Kristo Kontu"
+    println(kitapAdi)
+    val yayinlanmaTarihi = "10/01/2015"
+    println(yayinlanmaTarihi)
+    val indirimMiktari = 30.0
+    println(indirimMiktari)
+    val odaSayisi = 3
+    println(odaSayisi)
+    val enlem = 32.2
+    println(enlem)
+    val boylam =30.5
+    println(boylam)
+    val urunAdi = "Kitap"
+    println(urunAdi)
+    val yemekFiyati = 100
+    println(yemekFiyati)
+    val marka = "Adidas"
+    println(marka)
+    val muzikAdi = "Set Fire to the Rain"
+    println(muzikAdi)
+    val videoSuresi = 14.39
+    println(videoSuresi)
+    val urunPuani = 5
+    println(urunPuani)
+    val resimAdi = "Çığlık"
+    println(resimAdi)
+    val dosyaFormati = "pdf"
+    println(dosyaFormati)
+    val renk = "Mavi"
+    println(renk)
+    val renkKodu = "#000000"
+    println(renkKodu)
+    val telefonModeli = "IPhone 11"
+    println(telefonModeli)
+    val ekranBoyutu = 13.4
+    println(ekranBoyutu)
+    val agirlik = 15
+    println(agirlik)
+    val ulusalGun = "11 Kasım"
+    println(ulusalGun)
+    val tatilGunu = "Hafta Sonları"
+    println(tatilGunu)
+    val rezervasyonTarihi = "11/08/2024"
+    println(rezervasyonTarihi)
+    val sokakAdi = "altuntas sokak"
+    println(sokakAdi)
+    val otobusHatti = "100T"
+    println(otobusHatti)
+    val kalanDakika = 10
+    println(kalanDakika)
+    val takipKodu = "100A22S3D"
+    println(takipKodu)
+    val kuponSuresi = "11/10/2024"
+    println(kuponSuresi)
+    val kuponKodu = "INDIRIM100"
+    println(kuponKodu)
+    val faturaAdresi = "Melikgazi/Kayseri"
+    println(faturaAdresi)
+
+
+}
